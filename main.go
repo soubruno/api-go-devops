332581a3088dfaf6c3f292ffbb39fe0f76255453
soubruno <brunosantanasouza17@gmail.com>
2026-09-14 17:53:38 -0300

    fix: add in-memory fallback when database environment variables are not provided

--- main.go
+++ main.go
@@ -14,42 +14,50 @@ import (
 )
 
 func main() {
+	var repo user.UserRepository
+
 	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("DB_NAME")
-
+	dbPort := os.Getenv("DB_PORT")
 	if dbPort == "" {
 		dbPort = "5432"
 	}
 
-	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		dbHost, dbPort, dbUser, dbPassword, dbName)
+	// Se as variaveis de banco estiverem presentes, conecta ao Postgres
+	if dbHost != "" && dbUser != "" && dbPassword != "" && dbName != "" {
+		connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			dbHost, dbPort, dbUser, dbPassword, dbName)
 
-	var db *sql.DB
-	var err error
+		var db *sql.DB
+		var err error
 
-	// Loop de retry para aguardar o container do PostgreSQL estar pronto para conexões
-	for i := 0; i < 15; i++ {
-		db, err = sql.Open("postgres", connStr)
-		if err == nil {
-			err = db.Ping()
+		for i := 0; i < 15; i++ {
+			db, err = sql.Open("postgres", connStr)
 			if err == nil {
-				log.Println("Conexao com PostgreSQL realizada com sucesso!")
-				break
+				err = db.Ping()
+				if err == nil {
+					log.Println("Conexao com PostgreSQL realizada com sucesso!")
+					break
+				}
 			}
+			log.Printf("Aguardando banco de dados responder... tentativa %d/15\n", i+1)
+			time.Sleep(2 * time.Second)
 		}
-		log.Printf("Aguardando banco de dados responder... tentativa %d/15\n", i+1)
-		time.Sleep(2 * time.Second)
+
+		if err != nil {
+			log.Fatalf("Nao foi possivel conectar ao PostgreSQL: %v", err)
+		}
+		defer db.Close()
+
+		repo = user.NewUserRepository(db)
+	} else {
+		// Fallback em memoria para staging no Render ou execucao isolada
+		log.Println("Variaveis de banco nao detectadas. Inicializando repositorio em memoria.")
+		repo = user.NewInMemoryUserRepository()
 	}
 
-	if err != nil {
-		log.Fatalf("Nao foi possivel conectar ao PostgreSQL: %v", err)
-	}
-	defer db.Close()
-
-	repo := user.NewUserRepository(db)
 	service := user.NewUserService(repo)
 	controller := user.NewUserController(service)
 
@@ -84,6 +92,6 @@ func main() {
 		port = "3000"
 	}
 
-	log.Printf("Server running on http://localhost:%s\n", port)
+	log.Printf("Server running on port %s\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
--- user/repository.go
+++ user/repository.go
@@ -8,6 +8,63 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// --- Repositório em Memória (Fallback para Staging / Render) ---
+
+type InMemoryUserRepository struct {
+	users  []User
+	nextID int
+}
+
+func NewInMemoryUserRepository() UserRepository {
+	return &InMemoryUserRepository{
+		users:  []User{},
+		nextID: 1,
+	}
+}
+
+func (r *InMemoryUserRepository) GetAll() ([]User, error) {
+	return r.users, nil
+}
+
+func (r *InMemoryUserRepository) GetByID(id int) (*User, error) {
+	for _, u := range r.users {
+		if u.ID == id {
+			return &u, nil
+		}
+	}
+	return nil, errors.New("user not found")
+}
+
+func (r *InMemoryUserRepository) Create(u User) (*User, error) {
+	u.ID = r.nextID
+	r.nextID++
+	r.users = append(r.users, u)
+	return &u, nil
+}
+
+func (r *InMemoryUserRepository) Update(id int, u User) (*User, error) {
+	for i, user := range r.users {
+		if user.ID == id {
+			u.ID = id
+			r.users[i] = u
+			return &u, nil
+		}
+	}
+	return nil, errors.New("user not found")
+}
+
+func (r *InMemoryUserRepository) Delete(id int) error {
+	for i, u := range r.users {
+		if u.ID == id {
+			r.users = append(r.users[:i], r.users[i+1:]...)
+			return nil
+		}
+	}
+	return errors.New("user not found")
+}
+
+// --- Repositório PostgreSQL ---
+
 type PostgresUserRepository struct {
 	db *sql.DB
 }
